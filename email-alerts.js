54b2490796d963a654e5e5414cbd5e64f97b16df
omgimanerd <alvin.lin.dev@gmail.com>
2016-09-12 18:31:22 -0400

    initial commit

---
+++ email-alerts.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview This is an npm module that allows functions to be wrapped with a handler that will
+ * send email alerts on error.
+ * @author alvin.lin.dev@gmail.com (Alvin Lin)
+ */
+
+module.exports = {
+  
+};
