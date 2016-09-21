e03d7ea5a880726b6c61d8db04193b60cbe493a1
omgimanerd <alvin.lin.dev@gmail.com>
2016-09-21 16:11:18 -0400

    updated unit tests and added .npmignore

--- test/test.js
+++ test/test.js
@@ -5,59 +5,73 @@
 
 var expect = require('expect.js');
 
-describe('test for required options', function() {
-  it('should throw an error', function() {
-    expect(function() {
-      var emailAlerts = require('../email-alerts')({
-        apiKey: process.env.SENDGRID_API_KEY
-      });
-    }).to.throwError();
-  });
-
-  it('should throw an error', function() {
-    expect(function() {
-      var emailAlerts = require('../email-alerts')({
-        toEmail: 'blah'
-      });
-    }).to.throwError();
-  });
-
-  it('should not throw an error', function() {
-    expect(function() {
-      var emailAlerts = require('../email-alerts')({
-        toEmail: 'blah',
-        apiKey: process.env.SENDGRID_API_KEY
-      });
-    }).to.not.throwError();
-  });
-});
-
-describe('test the alert() method', function() {
-  it('should send me an email', function(done) {
-    var emailAlerts = require('../email-alerts')({
-      toEmail: 'alvin.lin.dev@gmail.com',
-      apiKey: process.env.SENDGRID_API_KEY
+describe('fail if required options not specified', function() {
+  describe('leaving out the toEmail option', function() {
+    it('should throw an error', function() {
+      expect(function() {
+        var emailAlerts = require('../email-alerts')({
+          apiKey: process.env.SENDGRID_API_KEY
+        });
+      }).to.throwError();
     });
-    var date = new Date();
-    var content = 'Unit test run at ' + date;
-    emailAlerts.alert('email-alerts unit test', content, function(error) {
-      expect(error).to.not.be.ok();
-      done();
+  });
+
+  describe('leaving out the apiKey option', function() {
+    it('should throw an error', function() {
+      expect(function() {
+        var emailAlerts = require('../email-alerts')({
+          toEmail: 'blah'
+        });
+      }).to.throwError();
+    });
+  });
+
+  describe('properly passing all required options', function() {
+    it('should not throw an error', function() {
+      expect(function() {
+        var emailAlerts = require('../email-alerts')({
+          toEmail: 'blah',
+          apiKey: process.env.SENDGRID_API_KEY
+        });
+      }).to.not.throwError();
     });
   });
 });
 
-describe('test the errorHandler() method', function() {
-  it('should send me an email', function(done) {
-    var emailAlerts = require('../email-alerts')({
-      toEmail: 'alvin.lin.dev@gmail.com',
-      apiKey: process.env.SENDGRID_API_KEY
+describe('test the module methods', function() {
+  var emailAlerts = require('../email-alerts')({
+    toEmail: 'alvin.lin.dev@gmail.com',
+    apiKey: process.env.SENDGRID_API_KEY
+  });
+
+  describe('test the alert() method', function() {
+    it('should send me an email', function(done) {
+      var date = new Date();
+      var content = 'Unit test run at ' + date;
+      emailAlerts.alert('email-alerts unit test', content, function(error) {
+        expect(error).to.not.be.ok();
+        done();
+      });
     });
-    emailAlerts.errorHandler(function(error, data, data2) {
-      expect(error).to.be(true);
-      expect(data).to.be('your mom');
-      expect(data2).to.be('your dad');
-      done();
-    })(true, 'your mom', 'your dad');
+  });
+
+  describe('test the errorHandler() method', function() {
+    it('should send me an email', function(done) {
+      emailAlerts.errorHandler(function(error, arg1, arg2) {
+        expect(error).to.be(true);
+        expect(arg1).to.be('your mom');
+        expect(arg2).to.be('your dad');
+        done();
+      })(true, 'your mom', 'your dad');
+    });
+
+    it('should not send me an email', function(done) {
+      emailAlerts.errorHandler(function(error, arg1, arg2) {
+        expect(error).to.be(false);
+        expect(arg1).to.be('fuck');
+        expect(arg2).to.be('gay');
+        done();
+      })(false, 'fuck', 'gay');
+    });
   });
 });
